a16f74219e5e1e0e7c99193a873aa156530d43e5
Renato Oliveira <renatooliveira.cin@gmail.com>
2012-09-24 01:49:46 -0300

    Adicionando Migrations

---
+++ apps/events/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Event'
+        db.create_table('events_event', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('full_description', self.gf('django.db.models.fields.TextField')()),
+            ('date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
+        ))
+        db.send_create_signal('events', ['Event'])
+
+    def backwards(self, orm):
+        # Deleting model 'Event'
+        db.delete_table('events_event')
+
+    models = {
+        'events.event': {
+            'Meta': {'object_name': 'Event'},
+            'date': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'full_description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['events']
