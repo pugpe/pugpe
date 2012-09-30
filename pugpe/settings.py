a9503c9d3b1eb8cb12590f1f9d838edb1b390a16
Renato Oliveira <renatooliveira.cin@gmail.com>
2012-09-29 23:46:15 -0300

    Adicionada pasta de templates.

--- pugpe/settings.py
+++ pugpe/settings.py
@@ -114,6 +114,7 @@ ROOT_URLCONF = 'pugpe.urls'
 WSGI_APPLICATION = 'pugpe.wsgi.application'
 
 TEMPLATE_DIRS = (
+    join(PROJECT_ROOT, 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
