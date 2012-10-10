70fbe1ad02fd92232fc7529212c2446cac3ba1ac
Fernando Rocha <fernandogrd@yahoo.com.br>
2012-10-10 17:05:10 -0300

    Corrigido disposição de pratocinadores e apoio
    Signed-off-by: Fernando Rocha <fernandogrd@yahoo.com.br>

--- apps/events/views.py
+++ apps/events/views.py
@@ -2,14 +2,19 @@
 from django.views.generic import ListView
 
 from .models import Event, EventTalk
+from .models import Support, Sponsor
 
 
 class EventMixin(object):
     '''Add event to context of views that require an event'''
     def get_context_data(self, **kwargs):
         event = Event.objects.get(slug=self.kwargs['event_slug'])
+        sponsors = Support.objects.filter(event=event)
+        supporters = Support.objects.filter(event=event)
 
-        kwargs.update({'event': event})
+        kwargs.update(
+            {'event': event, 'sponsors': sponsors, 'supporters': supporters},
+        )
         return super(EventMixin, self).get_context_data(**kwargs)
 
 
