28f28c68f5ed2d558cc9e80f1ad4ad770d6b55d3
Filipe Ximenes <filipeximenes@gmail.com>
2012-11-24 21:00:40 -0300

    automatic redirect to page given slug #6

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import ListView
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 
 import datetime
 from django.utils.timezone import utc
@@ -11,19 +11,26 @@ from .models import Support, Sponsor
 
 class EventMixin(object):
     '''Add event to context of views that require an event'''
-    def get_context_data(self, **kwargs):
-        event = get_object_or_404(Event, slug=self.kwargs['event_slug'])
-        sponsors = Sponsor.objects.filter(event=event)
-        supporters = Support.objects.filter(event=event)
+    event = None
+    sponsors = None
+    supporters = None
 
+    def dispatch(self, request, *args, **kwargs):
+        self.event = get_object_or_404(Event, slug=kwargs['event_slug'])
+        self.sponsors = Sponsor.objects.filter(event=self.event)
+        self.supporters = Support.objects.filter(event=self.event)
+
+        return super(EventMixin, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
         now = datetime.datetime.utcnow().replace(tzinfo=utc)
 
         allow_submission = False
-        if event.submission_deadline > now:
+        if self.event.submission_deadline > now:
             allow_submission = True
 
         kwargs.update(
-            {'event': event, 'sponsors': sponsors, 'supporters': supporters,
+            {'event': self.event, 'sponsors': self.sponsors, 'supporters': self.supporters,
             'allow_submission': allow_submission, },
         )
 
@@ -35,6 +42,15 @@ class TalkListView(EventMixin, ListView):
     template_name = u'events/event.html'
     context_object_name = 'talks'
 
+    def dispatch(self, request, *args, **kwargs):
+        reponse = super(TalkListView, self).dispatch(request, *args, **kwargs)
+
+        if not self.event.external_link:
+            return reponse
+        else:
+            return redirect(self.event.external_link)
+
     def get_queryset(self):
         qs = EventTalk.active.filter(event__slug=self.kwargs['event_slug'])
+
         return qs.order_by('start')
