12233b2fe3fb9364507750cf8cb6dde0f3b57a89
Fernando Rocha <fernandogrd@yahoo.com.br>
2012-12-22 16:06:36 -0300

    Adicionado migração vazia para resolver conflito de migrações
    Signed-off-by: Fernando Rocha <fernandogrd@yahoo.com.br>

---
+++ apps/events/migrations/0018_merge.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        pass
+    def backwards(self, orm):
+        pass
+    models = {
+        'cert.signature': {
+            'Meta': {'object_name': 'Signature'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'signature': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '80'})
+        },
+        'events.event': {
+            'Meta': {'object_name': 'Event'},
+            'date': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'external_link': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'full_description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'index': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'length': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geo.Location']", 'null': 'True', 'blank': 'True'}),
+            'partners': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['events.Partner']", 'null': 'True', 'blank': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'signature': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cert.Signature']", 'null': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'submission_deadline': ('django.db.models.fields.DateTimeField', [], {})
+        },
+        'events.eventtalk': {
+            'Meta': {'object_name': 'EventTalk'},
+            'end': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'event': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['events.Event']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'start': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'talk': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['submission.Talk']", 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '80', 'null': 'True', 'blank': 'True'})
+        },
+        'events.partner': {
+            'Meta': {'object_name': 'Partner'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        'events.sponsor': {
+            'Meta': {'object_name': 'Sponsor', '_ormbases': ['events.Partner']},
+            'partner_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['events.Partner']", 'unique': 'True', 'primary_key': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'events.support': {
+            'Meta': {'object_name': 'Support', '_ormbases': ['events.Partner']},
+            'partner_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['events.Partner']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'geo.location': {
+            'Meta': {'object_name': 'Location'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': "'50'"}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'district': ('django.db.models.fields.CharField', [], {'max_length': "'255'"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'map': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': "'15'"}),
+            'postal_code': ('django.db.models.fields.CharField', [], {'max_length': "'50'"}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'reference': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': "'50'"}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'submission.talk': {
+            'Meta': {'object_name': 'Talk'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '254'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'macro_theme': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '14'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'summary': ('django.db.models.fields.TextField', [], {}),
+            'talk_once': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        }
+    }
+
+    complete_apps = ['events']
